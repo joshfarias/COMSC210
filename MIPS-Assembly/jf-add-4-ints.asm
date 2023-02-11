73884b446a028d1ed457295dc3ccd4ba5aa9248c
Josh Farias <116445946+joshfarias@users.noreply.github.com>
2023-02-10 23:48:37 -0500

    Prompt user for 4 integers, add them and return result
    Prompt user for 4 integers, add them and return result

--- MIPS-Assembly/jf-add-4-ints.asm
+++ MIPS-Assembly/jf-add-4-ints.asm
@@ -8,7 +8,7 @@
 .data
 prompt1:	.asciiz "Enter a Number: "          
 prompt2:	.asciiz "Enter a Second Number: "     
-prompt3:        .asciiz "Enter a Third Number: "
+prompt3:  	.asciiz "Enter a Third Number: "
 prompt4:	.asciiz "Enter a Fourth Number: "
 result:		.asciiz "The Sum is: "
 crlf:		.asciiz "\n"
@@ -56,18 +56,63 @@ move $t3, $v0		#moves contents of $t3 to $v0
 add $t0, $t0, $t1	#adds $t0 and $t1 and stores result in $t0
 add $t0, $t0, $t2	#adds $t0 and $t2 and stores the result in $t0
 add $t0, $t0, $t3	#adds $t0 and $t3 and stores the result in $t0
+	
+la $a0, result		#loads in result prompt
+li $v0, 4		#load value of 4 into address $v0
+syscall			#calls system function 4 to print result prompt
+		
+li $v0, 1		#prints integer stored in $v0
+move $a0, $t0		#moves value of $a0 to $t0
+syscall
+		
+li  $v0, 4		#loads value of 4 into $v0
+la $a0, crlf		#loads the address of crlf into $a0
+syscall
+		
+li $v0, 10		#calls system function 10 to terminate program
+syscall
+
+li $v0, 4		#load value of 4 into address $v0
+syscall			#calls system function 4 to print prompt2
+		
+li $v0, 5		#reads integer entered
+syscall
+		
+move $t1, $v0		#moves contents of $t1 to $v0
+		
+la $a0, prompt3		#loads in prompt3
+la $v0, 4		#load value of 4 into address $v0
+syscall			#calls system function 4 to print prompt3
+		
+li $v0, 5		#reads integer entered
+syscall
+				
+move $t2, $v0		#moves contents of $t2 to $v0
+	
+la $a0, prompt4		#loads in prompt4
+li $v0, 4		#load value of 4 into $v0
+syscall			#calls system function 4 to print prompt4
+		
+li $v0, 5		#reads integer entered
+syscall
+				
+move $t3, $v0		#moves contents of $t3 to $v0
+		
+add $t0, $t0, $t1	#adds $t0 and $t1 and stores result in $t0
+add $t0, $t0, $t2	#adds $t0 and $t2 and stores the result in $t0
+add $t0, $t0, $t3	#adds $t0 and $t3 and stores the result in $t0
 		
 la $a0, result		#loads in result prompt
 li $v0, 4		#load value of 4 into address $v0
 syscall			#calls system function 4 to print result prompt
 		
-li $v0, 1		#
-move $a0, $t0
+li $v0, 1		#prints integer stored in $v0
+move $a0, $t0		#moves value of $a0 to $t0
 syscall
 		
-li  $v0, 4
-la $a0, crlf
+li  $v0, 4		#loads value of 4 into $v0
+la $a0, crlf		#loads the address of crlf into $a0
 syscall
 		
-li $v0, 10			#calls system function 10 to terminate program
+li $v0, 10		#calls system function 10 to terminate program
 syscall
